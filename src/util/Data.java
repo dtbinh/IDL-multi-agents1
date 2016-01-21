362a127cba480a82dbeab8a7cd98a85345492618
gisselFdez <gisselita04@hotmail.com>
2016-01-21 16:49:07 +0100

    feat(Data) adding poisson and requins information

--- src/util/Data.java
+++ src/util/Data.java
@@ -11,4 +11,9 @@ public class Data {
   public static Vue v;
   public static Boolean grilleVisible;
   public static Boolean equite; // shuffle ou non
+  public static int seedPoisson; //Nombre des tours un poisson doit attendre pour se reproduire
+  public static int seedRequin; //Nombre des tours un requin doit attendre pour se reproduire
+  public static int nombrePoissons;
+  public static int nombreRequins;
+  public static int tp; //numero du tp qui define la configuration de l'environement
 }
