7b2cc5dae4be4d2de0d2af28223a75f834ad80a3
gisselFdez <gisselita04@hotmail.com>
2016-01-21 16:49:51 +0100

    refactor(Main) adding configuration to run different tps

--- src/main/SmaMain.java
+++ src/main/SmaMain.java
@@ -13,19 +13,30 @@ public class SmaMain {
      * - taille de la bille
      * - vitesse
      * 
-     * Paramètres optionnels :
-     * - visibilité de la grille
-     * - équité -> shuffle
+     * Paramï¿½tres optionnels :
+     * - visibilitï¿½ de la grille
+     * - ï¿½quitï¿½ -> shuffle
      * - grille torique
      * - seed pour le Random (voir https://docs.oracle.com/javase/7/docs/api/java/util/Random.html#setSeed(long))
-     */
-
-    Data.size = 300;
-    Data.nombreAgents = 10500;
+     */	
+	
+    Data.size = 100;    
     Data.tours = 1000;
     Data.vitesse = 150;
     Data.grilleVisible = true;
     Data.equite = true;
+    Data.tp=1;
+    
+    //Donnes en fonction du tp
+    if(Data.tp==1)
+    	Data.nombreAgents = 500;
+    if(Data.tp==2){
+    	Data.seedPoisson = 2;
+        Data.seedRequin = 3;
+        Data.nombrePoissons=250;
+        Data.nombreRequins=250;
+    }
+    
 
     try {
       SMA sma = new SMA();
